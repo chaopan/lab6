158fb8685ee74a1c5c8a897afec6c27ff3b83104
chaopan <chaopan0329@gmail.com>
2016-02-11 05:40:50 +0000

    lab 6 done. commiting everything lol

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -20,19 +20,42 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
-	// Get the div ID, e.g., "project3"
+    
+    	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+    var url = "/project/" + idNumber;
+    console.log(url);
+    $.get("/project/" + idNumber, addProject);
+
+
 
 	console.log("User clicked on project " + idNumber);
 }
 
+function addProject(result){
+    console.log(result);
+    $("#project" + result.id + " .details").html(result.summary);
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+    $.get("/palette", changeColor);
+    
+}
+
+function changeColor(result){
+    
+    var colors = result.colors.hex;
+    
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
+    
 }
